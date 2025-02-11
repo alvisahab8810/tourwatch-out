14f11d4edcf19fcf6c2d761acdebd5d0ae418e1c
Riyaz Alvi <89968985+alvisahab8810@users.noreply.github.com>
2025-02-11 16:28:31 +0530

    changes done

--- pages/blogs.js
+++ pages/blogs.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Topbar from '../components/header/Header'
 import Footer from '../components/footer/Footer'
 import Hero from '../components/blogs/Hero'
-import About from '../components/blogs/about'
 import BlogList from '../components/blogs/BlogList'
+import About from '../components/blogs/About'
 
 export default function blogs() {
   return (
