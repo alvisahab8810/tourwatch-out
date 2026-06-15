49e3d8bba899aa672fd98ee399d54e0e41dc8b00
alvisahab8810 <alvisahab1999@gmail.com>
2026-06-15 17:10:53 +0530

    changes done

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WF5WRLK3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
